55d9cd6712ebe939d13ea6af7da0ccb0e22a3743
Nora Echeverry <nora.it270@gmail.com>
2018-11-16 11:53:50 -0500

    Commit all files

---
+++ FactoryPattern/IVehiculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public interface IVehiculo
+    {
+        string Nombre { get; set; }
+        int Puertas { get; set; }
+        int Ruedas { get; set; }
+        int Capacidad { get; set; }
+
+        string ObtenerInfo();
+    }
+}
